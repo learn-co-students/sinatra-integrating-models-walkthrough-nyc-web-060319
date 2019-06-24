64156907170c201895cbf7fb39db87cabc677ac3
Alexander Riccio <test35965+learn@gmail.com>
2019-06-24 15:22:04 -0400

    solve lab

--- app.rb
+++ app.rb
@@ -7,8 +7,11 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
-
+    @text_from_user = params[:user_text]
+    # @number_words = @text_from_user.split().length
+    # @vowels = @text_from_user.count("aeiou")
+    @analyzed_text = TextAnalyzer.new(@text_from_user)
+    @most = @analyzed_text.most_used_letter
     erb :results
   end
 end
