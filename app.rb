1da1d1b3d9fc91121724f1e6931b0919e49c3c1a
Tori Sigler <mssigler@gmail.com>
2019-06-25 16:45:33 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require_relative 'config/environment'
-require_relative 'models/text_analyzer.rb'
+require_relative 'models/text_analyzer.rb' 
+# gives us the ability to create new instances of the 
+# TextAnalyzer class from within our controller.
+
 
 class App < Sinatra::Base
   get '/' do
@@ -7,8 +10,12 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
+  	# text_from_user = params[:user_text]
+ 	# @analyzed_text = TextAnalyzer.new(text_from_user)
+ 	@analyzed_text = TextAnalyzer.new(params[:user_text])
 
-    erb :results
+
+    erb :results 		#renders the results page
+
   end
 end
